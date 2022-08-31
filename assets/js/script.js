c720bdac533317fc20affe652890332dbed62c83
NoahWest <noahwestberry@gmail.com>
2022-08-30 18:31:01 -0700

    Cleaned up html, rewrote js, adjusted css to fit new js and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,104 @@
-$(function () {});
-
-var today = moment().format("dddd, MMMM Do");
-var now = moment().format("H A");
-var planWorkday = [
-{ time: "9 AM", event: "" },
-{ time: "10 AM", event: "" },
-{ time: "11 AM", event: "" },
-{ time: "12 PM", event: "" },
-{ time: "1 PM", event: "" },
-{ time: "2 PM", event: "" },
-{ time: "3 PM", event: "" },
-{ time: "4 PM", event: "" },
-{ time: "5 PM", event: "" },
+var userBlocks = [];
+var hourBlocks = [
+  "09:00",
+  "10:00",
+  "11:00",
+  "12:00",
+  "13:00",
+  "14:00",
+  "15:00",
+  "16:00",
+  "17:00",
 ];
+//variable to save current time to reference if time slot has passed
+var currentTime = moment().format("HH") + ":00";
 
-var scheduledEvents = JSON.parse(localStorage.getItem("workDay"));
-if (scheduledEvents) {
-  planWorkday = scheduledEvents;
-}
+var domContainer = document.querySelector("#timeslot-container");
+//inserting time and day and time into Current day
 
-$("#currentDay").text(today);
+window.setInterval(function () {
+  $("#currentDay").text(moment().format("ddd MM/DD h:mm:ss a"));
+}, 1000);
 
-planWorkday.forEach(function(timeBlock, index) {
-	var timeLabel = timeBlock.time;
-	var blockColor = colorRow(timeLabel);
-	var row =
-		'<div class="time-block" id="' +
-		index +
-		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
-		timeLabel +
-		'</div><textarea class="form-control ' +
-		blockColor +
-		'">' +
-		timeBlock.event +
-		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
+//creates row to store time block
 
-	$(".container").append(row);
+var displayTimeBlocks = function () {
+    localTasks();
+
+  //for loop runs for each time stored in hourBlocks
+  for (i = 0; i < hourBlocks.length; i++) {
+    //creates div to hold task
+    var taskRow = document.createElement("div");
+    taskRow.classList = "row time-block justify-content-center";
+    //sets id of each elemnt to index value
+    taskRow.id = hourBlocks.indexOf(hourBlocks[i]);
+
+    //create timeslot
+
+    var timeSlot = document.createElement("h4");
+    timeSlot.classList = "hour col-md-2";
+    timeSlot.id = hourBlocks.indexOf(hourBlocks[i]);
+    timeSlot.textContent = hourBlocks[i];
+    //appending to task row
+    taskRow.appendChild(timeSlot);
+
+    //create input feild
+    var taskInput = document.createElement("input");
+    taskInput.classList = "time-block clearable col-md-9 description p-0";
+    taskInput.id = "input" + hourBlocks.indexOf(hourBlocks[i]);
+
+//space for local storage if statement to fill out info with parsed local storage
+if (userBlocks[i]) {
+    taskInput.value = userBlocks[i];
+  }
+  taskRow.appendChild(taskInput);
+
+//adds task info to middle of taskrow
+    taskRow.appendChild(taskInput);
+
+
+    //create save button
+    var saveBtn = document.createElement("button");
+    saveBtn.classList = "saveBtn col-md-1";
+    saveBtn.id = "btn" + hourBlocks.indexOf(hourBlocks[i]);
+    saveBtn.innerHTML = "<i class='far fa-save fa-lg'></i>";
+    
+    //adds save button to task row
+    taskRow.appendChild(saveBtn);
+
+//adding created elements to the form displayed
+    domContainer.appendChild(taskRow);
+
+    if (currentTime === hourBlocks[i]){
+    taskInput.classList = "present col-md-9 description p-0"
+    }
+    
+    if (currentTime < hourBlocks[i]){
+        taskInput.classList = "future col-md-9 description p-0"
+        }
+        
+    if (currentTime > hourBlocks[i]){
+        taskInput.classList = "past col-md-9 description p-0"
+        }
+  }
+};
+
+
+displayTimeBlocks();
+
+//code for save button
+$("button").on("click", function () {
+    var tempTask = [];
+    for (var i = 0; i < hourBlocks.length; i++){
+        tempTask.push(document.getElementsByTagName("input")[i].value);
+    }
+    userBlocks = tempTask;
+    localStorage.setItem("tasks", JSON.stringify(userBlocks));
 });
+
+//function to grab local storage
+function localTasks() {
+    if (JSON.parse(localStorage.getItem("tasks"))){
+        userBlocks = JSON.parse(localStorage.getItem("tasks"));
+    }
+}
